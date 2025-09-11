f72beb6d3fe38fbb1b3f79951542cf4ce0252d42
João Victor <lozeborges19@gmail.com>
2025-09-11 15:27:43 -0300

    so pra trocar de branch

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from .views import LoginView, HomeView # Importe a HomeView
+from .views import CustomSignupView
 
 urlpatterns = [
     # Esta linha define a página inicial
     path('', HomeView.as_view(), name='home'),
-
+    path("signup/", CustomSignupView.as_view(), name="account_signup"),
     path('login/', LoginView.as_view(), name='login'),
 ]
